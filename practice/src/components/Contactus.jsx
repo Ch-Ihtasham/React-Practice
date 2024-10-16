f8bc920cfdd223dca5bb8c0c161a013c4bf774b5
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-16 19:27:34 +0500

    commit

---
+++ practice/src/components/Contactus.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contactus() {
+  return (
+    <div>
+        this is contactus page
+      
+    </div>
+  )
+}
--- practice/src/main.jsx
+++ practice/src/main.jsx
@@ -8,9 +8,11 @@ import Um from './components/Um'
 import StopWatch from './components/StopWatch'
 import Button, { Btn3 } from './components/Button'
 import App from './App'
+import { Link } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
 
   <div>
+ 
     <App />
     {/* <Us /> */}
     {/* <Ue/> */}
