2fc52e5f10d90f1d12e8045165b1c3d861ea08bb
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-18 15:49:30 +0500

    commit

--- routing/src/main.jsx
+++ routing/src/main.jsx
@@ -4,22 +4,35 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Contact from './pages/Contact'
+import Blog from './pages/Blog'
+import App from './App'
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home/>
+      },
+      {
+        path: '/about',
+        element: <About />
 
-  },
-  {
-    path: '/about',
-    element: <About />
-
-  },
-  {
-    path: '/blog',
-    element: <About />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/blog',
+        element: <Blog/>
+      }
+    ]
 
   }
+
 ])
 createRoot(document.getElementById('root')).render(
 
