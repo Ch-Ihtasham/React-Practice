626dc25cd512de2ea0f0b7b2aa2446e3026d638e
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-19 12:41:47 +0500

    navlink

--- routing/src/Components/Navbar/Navbar.jsx
+++ routing/src/Components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import { Link, NavLink } from 'react-router-dom'
 export default function Navbar() {
     return (
         <div>
-            <ul>
-                <li> <Link to='/'>Home</Link> </li>
-                <li> <Link to='/blog'>Blog</Link> </li>
-                <li> <Link to='/contact'>Contact Us</Link> </li>
-                <li> <Link to='/about' >About</Link> </li>
+            <ul className='flex gap-7'>
+                <li> <NavLink className={({ isActive }) => `${isActive ? 'text-yellow-600' : 'text-black'} `} to='/'>Home</NavLink> </li>
+                <li> <NavLink className={({ isActive }) => `${isActive ? 'text-yellow-600' : 'text-black'}`} to='/blog'>Blog</NavLink> </li>
+                <li> <NavLink className={({ isActive }) => `${isActive ? 'text-yellow-600' : 'text-black'}`} to='/contact'>Contact Us</NavLink> </li>
+                <li> <NavLink className={({ isActive }) => `${isActive ? 'text-yellow-600' : 'text-black'}`} to='/about' >About</NavLink> </li>
 
             </ul>
         </div>
