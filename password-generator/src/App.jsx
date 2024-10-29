c6b63bdf1748c4f8592b112f44ab52a97d739586
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-29 19:27:31 +0500

    commit

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -5,18 +5,27 @@ function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
-  cosnt[password, setPassword] = useState('')
+  const [password, setPassword] = useState('')
   const passwordGenerator = useCallback(() => {
     let pass = ''
     let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     if (numberAllowed) str += '0123456789'
     if (charAllowed) str += '!@#$%&*?'
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor((Math.random() * str.length) + 1)
+      pass += str.charAt(char)
+
+    }
+    console.log(pass)
+    
 
   }, [length, numberAllowed, charAllowed, setPassword])
+  passwordGenerator()
 
 
   return (
     <>
+      {password}
 
     </>
   )
