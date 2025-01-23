10e410a0fc39cfe291d6b09669ba6fd4766e78c4
Ch Ihtasham <cihtasham32@gmail.com>
2025-01-23 12:50:25 +0500

    first commit

--- currency-changer/src/App.jsx
+++ currency-changer/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import Usman from './components/Usman'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Usman />
     </>
   )
 }
---
+++ currency-changer/src/components/Usman.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Usman() {
+  return (
+    <div>
+      Usman saeed
+    </div>
+  )
+}
